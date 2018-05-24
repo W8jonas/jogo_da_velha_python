ce59550582e6760ff597d3e160efbdf4475b072d
w8jonas <jonashenrique989@gmail.com>
2018-05-24 20:54:18 -0300

    tabuleiro completo usando vetor

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -4,18 +4,54 @@ import sys
 import random
 import time
 
-#####   Variaveis   #####
+#####   Variaveis   ##### imagem_de_fundo = pygame.image.load('imagens/jogo_da_velha_modelo_3.png')
 
-largura = 600
-altura = 470
+largura = 400
+altura = 400
+preto = (0, 0, 0)
+branco = (255, 255, 255)
+ponto_mouse_cor = (130, 130, 130)
+tamanho = largura / 3.334
+posicoes = []
 
-imagem_de_fundo = pygame.image.load('imagens/jogo_da_velha_modelo_3.png')
-
+imagem_de_fundo = preto
 pygame.init()
 relogio = pygame.time.Clock()
 
 jogador_1 = input(print('Escolha X ou O'))
 jogador_1 = jogador_1.upper()
+
+def jogo():
+    global posicoes
+    tela = pygame.display.set_mode([largura, altura])
+    tela.fill(imagem_de_fundo)
+    cont_posi = 0
+    for i in range(0, 3):
+        posicoes.append(pygame.Rect(largura / 40 + (tamanho + largura / 40) * i, altura / 40, tamanho, tamanho))
+        cont_posi += 1
+    cont_posi = 0
+    for i in range(4, 7):
+        posicoes.append(pygame.Rect(largura / 40 + (tamanho + largura / 40) * cont_posi, altura / 40 + (tamanho + altura / 40), tamanho, tamanho))
+        cont_posi += 1
+    cont_posi = 0
+    for i in range(7, 10):
+        posicoes.append(pygame.Rect(largura / 40 + (tamanho + largura / 40) * cont_posi, altura / 40 + (tamanho + altura / 40)*2, tamanho, tamanho))
+        cont_posi += 1
+
+    ponto_mouse = pygame.Rect(100, 100, 1, 1)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        for i in range(0, 9):
+            pygame.draw.rect(tela, branco, posicoes[i])
+        pygame.draw.rect(tela, ponto_mouse_cor, ponto_mouse)
+        pygame.display.update()
+        relogio.tick(50)
+
+
+jogo()
 
 if jogador_1 == 'X' or jogador_1 == 'O':
 
@@ -33,23 +69,3 @@ else:
 print(f'O jogador 1 fica com {jogador_1}')
 print(f'E o jogador 2 fica com {jogador_2}')
 
-
-def jogo():
-    tela = pygame.display.set_mode([largura, altura])
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
-        tela.blit(imagem_de_fundo, (0, 0))
-        pygame.display.update()
-        relogio.tick(50)
-
-jogo()
-
-# ler valor da posição do mouse e do click, cada hora alterar entre X ou O e marcar numa matriz os valores escritos
-# depois comparar os valores da matriz com os valores para ganhar.
-# finalizar o projeto
-# IDEIA NOVA
-# Usar fundo preto e colocar cada quadrado branco formando o tabuleiro
